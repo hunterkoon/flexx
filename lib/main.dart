7f82697c03a0d5097b3cbd76c0288169cff784b3
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2024-04-11 00:02:44 -0300

    refatoração widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: const Column(
-          children: [
-            Card(
-              color: Colors.grey,
-              child: ListTile(
-                title: Text(
-                  "Entrega 1",
-                  style: TextStyle(color: Colors.white),
-                ),
-                leading: Icon(Icons.monetization_on),
-              ),
-            ),
-            Card(
-              color: Colors.grey,
-              child: ListTile(
-                title: Text(
-                  "Entrega 2",
-                  style: TextStyle(color: Colors.white),
-                ),
-                leading: Icon(Icons.monetization_on),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(
           backgroundColor: Colors.red,
           title: const Text(
@@ -37,7 +14,9 @@ void main() {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            print("Clicou");
+          },
           backgroundColor: Colors.red,
           child: const Icon(
             Icons.add,
@@ -47,4 +26,41 @@ void main() {
       ),
     ),
   );
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: <Widget>[
+        EntregaValor('Entrega 1', '1'),
+      ],
+    );
+  }
+}
+
+class EntregaValor extends StatelessWidget {
+  final String entrega;
+  final String valor;
+
+  const EntregaValor(this.entrega, this.valor, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.grey,
+      child: ListTile(
+        title: Text(
+          entrega,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        leading: const Icon(
+          Icons.monetization_on,
+        ),
+        subtitle: Text('R\$$valor'),
+      ),
+    );
+  }
 }
