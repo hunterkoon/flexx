0267c4a9c417fcfa3169c7e4d346250001b17d10
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2024-04-18 01:57:05 -0300

    adicionando formulários

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const FlexxApp());
+}
+
+class FlexxApp extends StatelessWidget {
+  const FlexxApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: ListaTransferencias(),
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: const Text(
-            style: TextStyle(color: Colors.white),
-            "Flexx - Entregas Rápidas",
-            textDirection: TextDirection.ltr,
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("Clicou");
-          },
-          backgroundColor: Colors.red,
-          child: const Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-        ),
+        body: FormularioTransferencia(),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext buildContext) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de transferências!'),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+              decoration: InputDecoration(
+                  labelText: 'Número da conta', hintText: '0000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: TextField(
+              decoration:
+                  InputDecoration(
+                      icon: Icon(Icons.monetization_on),
+                      labelText: 'Valor', hintText: '100.00R\$'),
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Adicionar'))
+        ],
+      ),
+    );
+  }
 }
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: <Widget>[
-        EntregaValor('Entrega 1', '1'),
-      ],
+    return Scaffold(
+      body: const Column(
+        children: <Widget>[
+          EntregaValor("100", "100"),
+          EntregaValor("100", "100"),
+          EntregaValor("100", "100"),
+          EntregaValor("100", "100"),
+        ],
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text(
+          style: TextStyle(color: Colors.white),
+          "Flexx - Entregas Rápidas",
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
