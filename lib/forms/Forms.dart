5ec61f217a5d7cfc8be954f0eda30c26b9bf8012
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2024-04-19 00:54:36 -0300

    conhecendo os widgets

---
+++ lib/forms/Forms.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class Editor extends StatelessWidget {
+  final String title;
+  final String tip;
+  final TextEditingController controller;
+  final IconData icon;
+
+  const Editor(
+      {super.key,
+      required this.title,
+      required this.tip,
+      required this.controller,
+      required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: Colors.red,
+          ),
+          labelText: title,
+          hintText: tip,
+          floatingLabelStyle: const TextStyle(
+              backgroundColor: Colors.black, color: Colors.white),
+        ),
+        style: const TextStyle(
+          fontSize: 16,
+          color: Colors.black,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorNumeroConta = TextEditingController();
+  final TextEditingController _controladorValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext buildContext) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de transferências!'),
+      ),
+      body: Column(
+        children: [
+          Editor(
+              title: "Número da Conta",
+              tip: "0000",
+              controller: _controladorNumeroConta,
+              icon: Icons.numbers),
+          Editor(
+              title: "Valor da Transferência",
+              tip: "R\$ 200.00",
+              controller: _controladorValor,
+              icon: Icons.monetization_on),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              textStyle: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            onPressed: () {
+              debugPrint(_controladorNumeroConta.text);
+              debugPrint(_controladorValor.text);
+              final int? numeroConta =
+                  int.tryParse(_controladorNumeroConta.text);
+              final int? valorTransf =
+                  int.tryParse(_controladorValor.text);
+
+              try {
+                numeroConta != null && valorTransf != null
+                    ? Transferencia(numeroConta, valorTransf)
+                    : throw Exception("Valores de referência invalidos");
+              } on Exception catch (e) {
+                debugPrint(e.toString());
+              }
+            },
+            child: const Text('Adicionar'),
+          ),
+          //const Aviso(),
+        ],
+      ),
+    );
+  }
+}
+
+class Transferencia extends StatelessWidget {
+  final int? numeroConta;
+  final int? valor;
+
+  const Transferencia(this.numeroConta, this.valor, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.grey,
+      child: ListTile(
+        title: Text(
+          numeroConta.toString(),
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        leading: const Icon(
+          Icons.monetization_on,
+        ),
+        subtitle: Text('R\$$valor'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'forms/Forms.dart';
 
 void main() {
   runApp(const FlexxApp());
@@ -18,76 +22,7 @@ class FlexxApp extends StatelessWidget {
   }
 }
 
-class FormularioTransferencia extends StatelessWidget {
-  final TextEditingController _controladorCampoNumeroConta =
-      TextEditingController();
-  final TextEditingController _controladorCampoValor = TextEditingController();
 
-  static bool stateOfAlert = false;
-
-  @override
-  Widget build(BuildContext buildContext) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Lista de transferências!'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: TextField(
-              controller: _controladorCampoNumeroConta,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-              decoration: const InputDecoration(
-                  labelText: 'Número da conta', hintText: '0000'),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: TextField(
-              controller: _controladorCampoValor,
-              decoration: const InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Valor da transferência',
-                  hintText: '100.00 R\$'),
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              debugPrint(_controladorCampoNumeroConta.text);
-              debugPrint(_controladorCampoValor.text);
-
-              final int? numeroConta =
-                  int.tryParse(_controladorCampoNumeroConta.text);
-              final int? valorTransf =
-                  int.tryParse(_controladorCampoValor.text);
-
-              try {
-                numeroConta != null && valorTransf != null
-                    ? Transferencia(numeroConta, valorTransf)
-                    : throw Exception("Valores de referência invalidos");
-              } on Exception catch (e) {
-                debugPrint(e.toString());
-                FormularioTransferencia.stateOfAlert = true;
-              }
-            },
-            child: const Text('Adicionar'),
-          ),
-          //const Aviso(),
-        ],
-      ),
-    );
-  }
-}
 
 class Aviso extends StatelessWidget {
   const Aviso({super.key});
@@ -110,53 +45,29 @@ class Aviso extends StatelessWidget {
   }
 }
 
-class ListaTransferencias extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Column(
-        children: <Widget>[
-          Transferencia(100, 100),
-        ],
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: const Text(
-          style: TextStyle(color: Colors.white),
-          "Flexx - numeroContas Rápidas",
-          textDirection: TextDirection.ltr,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+// class ListaTransferencias extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: const Column(
+//         children: <Widget>[
+//           Transferencia(100, 100),
+//         ],
+//       ),
+//       appBar: AppBar(
+//         backgroundColor: Colors.red,
+//         title: const Text(
+//           style: TextStyle(color: Colors.white),
+//           "Flexx - numeroContas Rápidas",
+//           textDirection: TextDirection.ltr,
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
 
-class Transferencia extends StatelessWidget {
-  final int? numeroConta;
-  final int? valor;
 
-  const Transferencia(this.numeroConta, this.valor, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Colors.grey,
-      child: ListTile(
-        title: Text(
-          numeroConta.toString(),
-          style: const TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        leading: const Icon(
-          Icons.monetization_on,
-        ),
-        subtitle: Text('R\$$valor'),
-      ),
-    );
-  }
-}
